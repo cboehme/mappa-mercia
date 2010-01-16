2adab1fb3115a645349490d55954bf252bcaf62d
Christoph Böhme <christoph@b3e.net>
2010-01-16 10:38:55 +0000

    - serve now creates a link to the tiles directory

--- serve.sh
+++ serve.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 ln -sf ../osb-proxy ./website/osb-proxy
+ln  -sf ../../gritting/tiles ./website/tiles
+
 ./mongoose/mongoose -ssi_ext .shtml,.shtml.inc -cgi_ext addPOIexec,editPOIexec,closePOIexec,getBugs -root ./website
+
 rm -f ./website/osb-proxy
+rm -f ./website/tiles
